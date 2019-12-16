0479d5197ecb7734bbe368298cdf3c79256062d1
rheza <rheza@mospaze.com>
2019-12-16 15:14:13 +0700

    add base-layout

---
+++ components/Subheader.vue
@@ -0,0 +1,22 @@
+<template>
+    
+    <div class="kt-subheader   kt-grid__item" id="kt_subheader">
+        <div class="kt-container  kt-container--fluid ">
+            <div class="kt-subheader__main">
+                <h3 class="kt-subheader__title">
+                    Wizard 1 </h3>
+                <span class="kt-subheader__separator kt-hidden"></span>
+                <div class="kt-subheader__breadcrumbs">
+                    <a href="#" class="kt-subheader__breadcrumbs-home"><i class="flaticon2-shelter"></i></a>
+                    <span class="kt-subheader__breadcrumbs-separator"></span>
+                    <a href="" class="kt-subheader__breadcrumbs-link">
+                        Pages </a>
+                    <span class="kt-subheader__breadcrumbs-separator"></span>
+                    <a href="" class="kt-subheader__breadcrumbs-link">
+                        Wizard 1 </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,37 @@
 <template>
-  <div class="wrapper">
-    <navbar />
 
-    <sidebar />
+    <div class="kt-grid kt-grid--hor kt-grid--root">
+			<div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--ver kt-page">
+        <mobileHeader />
+        <sidebar />
+        <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor kt-wrapper" id="kt_wrapper">
+          <headers />
+          <div class="kt-content  kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor" id="kt_content">
+            <div class="kt-subheader  kt-grid__item" id="kt_subheader">
+              <subheader />
+            </div> 
+            <div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
+              <nuxt />
+            </div> 
+          </div> 
+          <footers />
+        </div>
 
-    <section class="content-wrapper">
-      <div class="content-heading">
-        <breadcrumb />
       </div>
+    </div>
 
-      <nuxt />
-    </section>
-  </div>
 </template>
 
 <script>
-import Navbar from '@/components/navbar/Navbar.vue'
 import Sidebar from '@/components/Sidebar.vue'
-import Breadcrumb from '@/components/Breadcrumbs.vue'
+import MobileHeader from '@/components/MobileHeader.vue'
+import Headers from '@/components/Header.vue'
+import Subheader from '@/components/Subheader.vue'
+import Footers from '@/components/Footer.vue'
 
 export default {
   components: {
-    Navbar, Sidebar, Breadcrumb,
+    Sidebar, MobileHeader, Headers, Subheader, Footers
   },
 }
 </script>
