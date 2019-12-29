6d763545aafff2e463a2e71ae2b80297eb305c02
rheza <rheza@mospaze.com>
2019-12-29 18:51:11 +0700

    add select2 validation

--- pages/warehouse/add.vue
+++ pages/warehouse/add.vue
@@ -126,7 +126,7 @@
                 >
                   <option />
                   <option
-                    v-for="country in countries"
+                    v-for="country in $store.state.region.countries"
                     :key="country.id"
                     :value="country.id"
                   >
@@ -143,6 +143,13 @@
                   name="state_id"
                 >
                   <option />
+                  <option
+                    v-for="state in states"
+                    :key="state.id"
+                    :value="state.id"
+                  >
+                    {{ state.name }}
+                  </option>
                 </select>
                 <span class="form-text text-muted">Please select a country </span>
               </div>
@@ -156,6 +163,13 @@
                   name="city_id"
                 >
                   <option />
+                  <option
+                    v-for="city in cities"
+                    :key="city.id"
+                    :value="city.id"
+                  >
+                    {{ city.name }}
+                  </option>
                 </select>
                 <span class="form-text text-muted">Please select a state </span>
               </div>
@@ -167,6 +181,13 @@
                   name="district_id"
                 >
                   <option />
+                  <option
+                    v-for="district in districts"
+                    :key="district.id"
+                    :value="district.id"
+                  >
+                    {{ district.name }}
+                  </option>
                 </select>
                 <span class="form-text text-muted">Please select a city </span>
               </div>
@@ -219,20 +240,157 @@ export default {
   head: { script: [{ src: '/js/warehouse/add.js', body: true }] },
   data () {
     return {
-      countries: [],
-      state    : [],
-      city     : [],
-      district : [],
+      states   : [],
+      cities   : [],
+      districts: [],
     }
   },
-  async created () {
-    await this.getCountry()
+  async fetch ({ store, params }) {
+    await store.dispatch('region/getCountries')
+  },
+  mounted () {
+    const app = this
+    $('[data-switch=true]').bootstrapSwitch()
+    $('[data-switch=true]').on('switchChange.bootstrapSwitch', function () {
+      validator.element($(this)) // validate element
+    })
+
     $('#country').select2({ placeholder: 'Select a country', allowClear: true })
+    $('#country').on('change', function () {
+      validator.element($(this))
+      if ($('#country').val()) {
+        $('#state').prop('disabled', false)
+        app.getStatesByCountry()
+      } else {
+        $('#state').val(null).trigger('change')
+        $('#city').val(null).trigger('change')
+        $('#district').val(null).trigger('change')
+        $('#state').prop('disabled', true)
+        $('#city').prop('disabled', true)
+        $('#district').prop('disabled', true)
+      }
+    })
+
+    $('#state').select2({
+      placeholder: 'Select a state', allowClear: true, disabled: true,
+    })
+    $('#state').on('change', function () {
+      validator.element($(this))
+      if ($('#state').val()) {
+        $('#city').prop('disabled', false)
+        app.getCitiesByState()
+      } else {
+        $('#city').val(null).trigger('change')
+        $('#district').val(null).trigger('change')
+        $('#city').prop('disabled', true)
+        $('#district').prop('disabled', true)
+      }
+    })
+
+    $('#city').select2({
+      placeholder: 'Select a city', allowClear: true, disabled: true,
+    })
+    $('#city').on('change', function () {
+      validator.element($(this))
+      if ($('#city').val()) {
+        $('#district').prop('disabled', false)
+        app.getDistrictsByCity()
+      } else {
+        $('#district').val(null).trigger('change')
+        $('#district').prop('disabled', true)
+      }
+    })
+
+    $('#district').select2({
+      placeholder: 'Select a district', allowClear: true, disabled: true,
+    })
+    $('#district').on('change', function () {
+      validator.element($(this))
+    })
+
+    const validator = $('#warehouse_form').validate({
+      // define validation rules
+      rules: {
+        name : { required: true },
+        code : { required: true },
+        email: {
+          required : true,
+          email    : true,
+          minlength: 10,
+        },
+        phone: {
+          required: true,
+          digits  : true,
+        },
+        capacity   : { required: true },
+        pic        : { required: true },
+        address    : { required: true },
+        country_id : { required: true },
+        state_id   : { required: true },
+        city_id    : { required: true },
+        district_id: { required: true },
+        zip_code   : {
+          required: true,
+          digits  : true,
+        },
+        location: { required: true },
+      },
+
+      // display error alert on form submit
+      invalidHandler: function (event, validator) {
+        const alert = $('#kt_form_1_msg')
+        alert.removeClass('kt--hide').show()
+        // eslint-disable-next-line no-undef
+        KTUtil.scrollTop()
+
+        event.preventDefault()
+      },
+
+      submitHandler: function (form) {
+        // form[0].submit(); // submit the form
+        return false
+      },
+    })
   },
   methods: {
-    async getCountry () {
-      await this.$store.dispatch('region/getCountry')
-      this.countries = this.$store.getters['region/getCountries']
+    async getStatesByCountryModel () {
+      await this.$store.dispatch('region/getStatesByCountry', { countryId: $('#country').val() })
+      this.states = this.$store.getters['region/getStatesByCountry']
+    },
+    async getStatesByCountry () {
+      await this.getStatesByCountryModel()
+      $('#country').select2({ placeholder: 'Select a country', allowClear: true })
+      $('#state').select2({ placeholder: 'Select a state', allowClear: true })
+      $('#city').select2({
+        placeholder: 'Select a city', allowClear: true, disabled: true,
+      })
+      $('#district').select2({
+        placeholder: 'Select a district', allowClear: true, disabled: true,
+      })
+    },
+    async getCitiesByStateModel () {
+      await this.$store.dispatch('region/getCitiesByState', { stateId: $('#state').val() })
+      this.cities = this.$store.getters['region/getCitiesByState']
+    },
+    async getCitiesByState () {
+      await this.getCitiesByStateModel()
+      $('#country').select2({ placeholder: 'Select a country', allowClear: true })
+      $('#state').select2({ placeholder: 'Select a state', allowClear: true })
+      $('#city').select2({ placeholder: 'Select a city', allowClear: true })
+      $('#district').select2({
+        placeholder: 'Select a district', allowClear: true, disabled: true,
+      })
+    },
+    async getDistrictsByCityModel () {
+      await this.$store.dispatch('region/getDistrictsByCity', { cityId: $('#city').val() })
+      this.districts = this.$store.getters['region/getDistrictsByCity']
+    },
+    async getDistrictsByCity () {
+      await this.getDistrictsByCityModel()
+      $('#country').select2({ placeholder: 'Select a country', allowClear: true })
+      $('#state').select2({ placeholder: 'Select a state', allowClear: true })
+      $('#city').select2({ placeholder: 'Select a city', allowClear: true })
+      $('#district').select2({ placeholder: 'Select a district', allowClear: true })
     },
   },
 }
