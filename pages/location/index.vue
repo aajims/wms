c64d845ba71bf380c9efc41db58819755ecdc770
rheza <rheza@mospaze.com>
2020-01-06 22:59:23 +0700

    add location list

--- pages/location/index.vue
+++ pages/location/index.vue
@@ -6,21 +6,35 @@
     <div class="kt-portlet__head kt-portlet__head--lg">
       <div class="kt-portlet__head-label">
         <span class="kt-portlet__head-icon">
-          <i class="kt-font-brand flaticon2-architecture-and-city" />
+          <i class="kt-font-brand flaticon2-shelter" />
         </span>
         <h3 class="kt-portlet__head-title">
-          Warehouse List
+          Location List
         </h3>
       </div>
       <div class="kt-portlet__head-toolbar">
         <div class="kt-portlet__head-wrapper">
           <div class="kt-portlet__head-actions">
             <a
-              href="/warehouse/add"
+              href="/location/add"
               class="btn btn-brand btn-elevate btn-icon-sm"
             >
               <i class="la la-plus" />
-              <span class="kt-hidden-mobile">Add Warehouse</span>
+              <span class="kt-hidden-mobile">Add Location</span>
+            </a>
+            <a
+              href="/location/import"
+              class="btn btn-brand btn-elevate btn-icon-sm"
+            >
+              <i class="la la-cloud-download" />
+              <span class="kt-hidden-mobile">Import Excel</span>
+            </a>
+            <a
+              href="/location/export"
+              class="btn btn-brand btn-elevate btn-icon-sm"
+            >
+              <i class="la la-file-excel-o" />
+              <span class="kt-hidden-mobile">Export Excel</span>
             </a>
           </div>
         </div>
@@ -30,9 +44,9 @@
       <!--begin: Search Form -->
       <div class="kt-form kt-form--label-right kt-margin-t-20 kt-margin-b-10">
         <div class="row align-items-center">
-          <div class="col-xl-8 order-2 order-xl-1">
+          <div class="col-xl-10 order-2 order-xl-1">
             <div class="row align-items-center">
-              <div class="col-md-4 kt-margin-b-20-tablet-and-mobile">
+              <div class="col-md-2 kt-margin-b-20-tablet-and-mobile">
                 <div class="kt-form__group">
                   <div class="kt-form__label">
                     <label>Filter By:</label>
@@ -43,13 +57,10 @@
                       class="form-control bootstrap-select"
                     >
                       <option value="name">
-                        Warehouse
+                        Location
                       </option>
-                      <option value="code">
-                        Code
-                      </option>
-                      <option value="address">
-                        Address
+                      <option value="level">
+                        Level
                       </option>
                     </select>
                   </div>
@@ -65,14 +76,14 @@
                     type="text"
                     class="form-control"
                     placeholder="Search..."
-                    @keyup="getWarehouse(params.page)"
+                    @keyup="getLocation(params.page)"
                   >
                   <span class="kt-input-icon__icon kt-input-icon__icon--left">
                     <span><i class="la la-search" /></span>
                   </span>
                 </div>
               </div>
-              <div class="col-md-3 kt-margin-b-20-tablet-and-mobile">
+              <div class="col-md-2 kt-margin-b-20-tablet-and-mobile">
                 <div class="kt-form__group">
                   <div class="kt-form__label">
                     <label>Status:</label>
@@ -95,6 +106,97 @@
                   </div>
                 </div>
               </div>
+              <div class="col-md-4 kt-margin-b-20-tablet-and-mobile">
+                <div class="kt-form__group">
+                  <div class="kt-form__label">
+                    <label>Warehouse:</label>
+                  </div>
+                  <div class="kt-form__control">
+                    <select
+                      id="kt_form_warehouse"
+                      class="form-control bootstrap-select"
+                    >
+                      <option value="">
+                        All
+                      </option>
+                      <option
+                        v-for="warehouse in warehouseData"
+                        :key="warehouse.id"
+                        :value="warehouse.id"
+                      >
+                        {{ warehouse.name }}
+                      </option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-2 order-1 order-xl-2 ">
+            <div class="kt-form__group">
+              <div class="kt-form__label">
+                <label>&nbsp;</label>
+              </div>
+              <div class="kt-form__control">
+                <a
+                  href="javascript:void(0)"
+                  class="btn btn-default"
+                  @click="clearForm"
+                >
+                  <i class="flaticon2-circular-arrow" /> Clear
+                </a>
+                <div class="kt-separator d-xl-none" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <br>
+        <div class="row align-items-center">
+          <div class="col-xl-8 order-2 order-xl-1">
+            <a
+              style="margin-top: 5px"
+              href="javascript:void(0)"
+              class="kt-badge kt-badge--inline kt-badge--md kt-badge--success"
+              @click="getLocationCapacity('=0')"
+            >Empty</a>
+            <a
+              style="margin-top: 5px"
+              href="javascript:void(0)"
+              class="kt-badge kt-badge--inline kt-badge--md kt-badge--info"
+              @click="getLocationCapacity('<50')"
+            >Below 50%</a>
+            <a
+              style="margin-top: 5px"
+              href="javascript:void(0)"
+              class="kt-badge kt-badge--inline kt-badge--md kt-badge--warning"
+              @click="getLocationCapacity('>=50')"
+            >Hit the Maximum Capacity 50%</a>
+            <a
+              style="margin-top: 5px"
+              href="javascript:void(0)"
+              class="kt-badge kt-badge--inline kt-badge--md kt-badge--danger"
+              @click="getLocationCapacity('>=90')"
+            >Hit the Maximum Capacity 90%</a>
+          </div>
+          <div class="col-xl-4 order-1 order-xl-2 ">
+            <div
+              style="margin-top: 5px"
+              class="kt-checkbox-inline"
+            >
+              <label class="kt-checkbox kt-checkbox--tick kt-checkbox--brand">
+                <input
+                  type="checkbox"
+                  @change="changeCheckBox('stock_quarantine', $event)"
+                > Stock Quarantine
+                <span />
+              </label>
+              <label class="kt-checkbox kt-checkbox--tick kt-checkbox--brand">
+                <input
+                  type="checkbox"
+                  @change="changeCheckBox('bonded_location', $event)"
+                > Bonded Location
+                <span />
+              </label>
             </div>
           </div>
         </div>
@@ -107,7 +209,7 @@
         style-class="vgt-table table-hover"
         :columns="columns"
         :rows="rows"
-        max-height="400px"
+        max-height="500px"
         :fixed-header="true"
         :is-loading="isLoading"
         @on-sort-change="onSortChange"
@@ -127,13 +229,13 @@
             class="actions"
           >
             <a
-              :href="'/warehouse/detail/' + props.row.id"
-              title="Warehouse Detail"
+              :href="'/location/detail/' + props.row.id"
+              title="Location Detail"
               class="btn btn-sm btn-clean btn-icon btn-icon-md"
             ><i class="la la-search" /></a>
             <a
-              :href="'/warehouse/edit/' + props.row.id"
-              title="Edit warehouse"
+              :href="'/location/edit/' + props.row.id"
+              title="Edit location"
               class="btn btn-sm btn-clean btn-icon btn-icon-md"
             ><i class="la la-edit" /></a>
             <a
@@ -156,6 +258,7 @@
         :to="to"
         :total-item="totalItem"
         :click-handler="clickCallback"
+        :per-page-dropdown="[10, 20, 30, 50, 100, 200, 500]"
         @changePerpage="changePerPage"
       />
     </client-only>
@@ -178,41 +281,50 @@ export default {
   components: { pagination },
   data () {
     return {
-      isLoading: false,
-      columns  : [
+      warehouseData: [],
+      isLoading    : false,
+      columns      : [
         {
           label   : '#',
           field   : 'lineNumber',
           sortable: false,
         },
         {
-          label: 'Name',
+          label   : '',
+          field   : 'indicator',
+          width   : '5px',
+          sortable: false,
+          tdClass : this.tdClassFunc,
+        },
+        {
+          label   : 'Warehouse',
+          field   : 'warehouse_name',
+          sortable: false,
+        },
+        {
+          label: 'Location',
           field: 'name',
         },
         {
-          label: 'Code',
-          field: 'code',
+          label: 'Level',
+          field: 'level',
         },
         {
-          label   : 'Address',
-          field   : 'address',
-          sortable: false,
+          label: 'Capacity',
+          field: 'capacity',
         },
         {
-          label   : 'Country',
-          field   : 'country_name',
-          tdClass : 'text-center',
-          sortable: false,
+          label: 'Usage',
+          field: 'usage',
+        },
+        {
+          label: 'Weight',
+          field: 'weight_max',
         },
         {
           label  : 'Status',
           field  : 'status',
           tdClass: 'text-center',
-        },
-        {
-          label   : 'Create By',
-          field   : 'created_by_name',
-          sortable: false,
         },
         {
           label           : 'Create Date',
@@ -231,11 +343,12 @@ export default {
       ],
       rows  : [],
       params: {
-        page    : 1,
-        per_page: 10,
-        sort_by : 'created_at',
-        sort    : 'desc',
-        keyword : '',
+        page     : 1,
+        per_page : 10,
+        sort_by  : 'created_at',
+        sort     : 'desc',
+        keyword  : '',
+        search_by: '',
       },
       totalPage: 0,
       totalItem: 0,
@@ -244,36 +357,66 @@ export default {
     }
   },
   async mounted () {
+    this.getLocation(this.params.page)
+    this.getWarehouse()
     const app = this
     $('#kt_form_filter').select2({ minimumResultsForSearch: -1, width: '100%' })
     $('#kt_form_status').select2({ minimumResultsForSearch: -1, width: '100%' })
     $('#kt_form_status').on('change', function () {
-      app.params['filter[status]'] = $('#kt_form_status').val()
-      app.getWarehouse(1)
+      if ($('#kt_form_status').val() !== '' && $('#kt_form_status').val() !== null)
+        app.params['filter[status]'] = $('#kt_form_status').val()
+      if ($('#kt_form_status').val() !== null)
+        app.getLocation(1)
     })
-    this.getWarehouse(this.params.page)
+    $('#kt_form_warehouse').on('change', function () {
+      if ($('#kt_form_warehouse').val() !== '' && $('#kt_form_warehouse').val() !== null)
+        app.params['filter[warehouse_id]'] = $('#kt_form_warehouse').val()
+      if ($('#kt_form_warehouse').val() !== null)
+        app.getLocation(1)
+    })
   },
   methods: {
+    getLocationCapacity (value) {
+      this.params.usage_percentage = value
+      this.getLocation(1)
+    },
+    changeCheckBox (index, event) {
+      if (event.target.checked === true)
+        this.params[`filter[${index}]`] = 1
+      else
+        this.$delete(this.params, `filter[${index}]`)
+      this.getLocation(1)
+    },
+    tdClassFunc (row) {
+      if (row.usage_percentage === 0)
+        return 'capacity-indicator-success'
+      if (row.usage_percentage !== 0 && row.usage_percentage < 50)
+        return 'capacity-indicator-info'
+      if (row.usage_percentage >= 50 && row.usage_percentage < 90)
+        return 'capacity-indicator-warning'
+      if (row.usage_percentage >= 90)
+        return 'capacity-indicator-danger'
+    },
     clickCallback (pageNumber) {
-      this.getWarehouse(pageNumber)
+      this.getLocation(pageNumber)
     },
     changePerPage (value) {
       this.params.per_page = value
-      this.getWarehouse(1)
+      this.getLocation(1)
     },
     onSortChange (params) {
       this.params.sort_by = params[0].field
       this.params.sort    = params[0].type
-      this.getWarehouse(1)
+      this.getLocation(1)
     },
-    async getWarehouse (page) {
-      this.params.search_by = $('#kt_form_filter').val()
+    async getLocation (page) {
       this.params.page      = page
+      this.params.search_by = $('#kt_form_filter').val()
       let data              = []
       try {
         this.isLoading = true
-        await this.$store.dispatch('warehouse/list', { params: this.params })
-        data           = this.$store.getters['warehouse/getWarehouse']
+        await this.$store.dispatch('location/list', { params: this.params })
+        data           = this.$store.getters['location/getLocation']
         this.totalPage = data.pagination.last_page
         this.totalItem = data.pagination.total
         this.from      = data.pagination.from
@@ -296,7 +439,7 @@ export default {
       // eslint-disable-next-line no-undef
       swal.fire({
         title             : 'Are you sure?',
-        text              : `Warehouse "${row.name}" ${statusText}`,
+        text              : `Location "${row.name}" in warehouse "${row.warehouse_name}" ${statusText}`,
         type              : 'question',
         showCancelButton  : true,
         confirmButtonText : statusText,
@@ -308,15 +451,15 @@ export default {
           app.updateStatus(row.id, row)
       })
     },
-    async updateStatus (idWarehouse, param) {
+    async updateStatus (idLocation, param) {
       try {
         this.$nuxt.$loading.start()
         param.status    = param.status === 1 ? 0 : 1
-        await this.$store.dispatch('warehouse/editWarehouse', { idWarehouse: idWarehouse, data: param })
-        const data      = this.$store.getters['warehouse/getEditWarehouse']
+        await this.$store.dispatch('location/editLocation', { idLocation: idLocation, data: param })
+        const data      = this.$store.getters['location/getEditLocation']
         const parameter = {
           alertClass: 'alert-success',
-          message   : `Warehouse ${data.result.name} has been edited`,
+          message   : `Location ${data.result.name} in warehouse ${data.result.warehouse_name} has been edited`,
         }
         this.$nuxt.$emit('alertShow', parameter)
         this.$nuxt.$loading.finish()
@@ -333,6 +476,36 @@ export default {
         // eslint-disable-next-line no-undef
         KTUtil.scrollTop()
       }
+    },
+    async getWarehouse () {
+      try {
+        const warehouseParams = {
+          'page'          : 1,
+          'per_page'      : 1000,
+          'status'        : '',
+          'sort_by'       : 'name',
+          'sort'          : 'asc',
+          'keyword'       : '',
+          'filter[status]': 1,
+        }
+        await this.$store.dispatch('warehouse/list', { params: warehouseParams })
+        $('#kt_form_warehouse').select2()
+        this.warehouseData    = this.$store.getters['warehouse/getWarehouse'].result
+      } catch (error) {
+        this.warehouseData = []
+      }
+    },
+    async clearForm () {
+      this.params = {
+        page    : 1,
+        per_page: 10,
+        sort_by : 'created_at',
+        sort    : 'desc',
+        keyword : '',
+      }
+      await this.getLocation(1)
+      $('#kt_form_status').val(null).trigger('change')
+      $('#kt_form_warehouse').val(null).trigger('change')
     },
   },
 }
