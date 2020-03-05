4cdb39d6d13b11ea32fdda948117c49277167cb3
rheza <rheza@mospaze.com>
2020-03-05 15:37:06 +0700

    fixing job incoming

--- pages/incoming/print/_id.vue
+++ pages/incoming/print/_id.vue
@@ -3,6 +3,10 @@
   border: 1px solid #ddd!important;
   padding: 5px;
 }
+#page:after {
+  counter-increment: page;
+  content: counter(page);
+}
 </style>
 
 <template>
@@ -10,91 +14,92 @@
     <div
       style="margin:1% !important; width:98%;"
     >
+      <h5>INCOMING STOCK</h5>
       <table width="100%">
         <tr>
-          <td
-            valign="top"
-            align="left"
-            width="50%"
-          >
-            <h4>INCOMING STOCK</h4>COMPANY: {{ incoming.company_name }}
+          <td style="width: 10%">
+            Company
           </td>
-          <td
-            valign="top"
-            align="right"
-            width="50%"
-          >
-            <h4>{{ incoming.job_no }}</h4>
+          <td style="width: 1%;">
+            :
           </td>
-        </tr>
-      </table>
-
-      <div style="margin-top: 20px;  margin-bottom: 20px; border: 0; border-top: 1px solid #eee;" />
-
-      <table class="table table-bordered">
-        <tr>
-          <td
-            valign="top"
-            width="150px"
-          >
-            Description
+          <td>
+            {{ incoming.company_name }}
           </td>
-          <td
-            valign="top"
-            width="150px"
-          >
-            ETD
+          <td style="width: 10%">
+            Order Number
           </td>
-          <td
-            valign="top"
-            width="150px"
-          >
-            Transport type
+          <td style="width: 1%;">
+            :
+          </td>
+          <td>
+            {{ incoming.order_no }}
           </td>
         </tr>
         <tr>
-          <td>{{ incoming.description }}</td>
-          <td>{{ etd }}</td>
-          <td>{{ transportType }}</td>
+          <td style="width: 10%">
+            Warehouse
+          </td>
+          <td style="width: 1%;">
+            :
+          </td>
+          <td>
+            {{ incoming.to_warehouse_name }}
+          </td>
+          <td style="width: 10%">
+            Created Date
+          </td>
+          <td style="width: 1%;">
+            :
+          </td>
+          <td>
+            {{ date(incoming.created_at) }}
+          </td>
         </tr>
-      </table>
-
-      <table class="table table-bordered">
         <tr>
-          <td
-            valign="top"
-            width="150px"
-          >
+          <td style="width: 10%">
+            Job Number
+          </td>
+          <td style="width: 1%;">
+            :
+          </td>
+          <td>
+            {{ incoming.job_no }}
+          </td>
+          <td style="width: 10%">
+            Page
+          </td>
+          <td style="width: 1%;">
+            :
+          </td>
+          <td>
+            <span id="page" />
+          </td>
+        </tr>
+        <tr>
+          <td style="width: 10%">
             From
           </td>
-          <td
-            valign="top"
-            width="150px"
-          >
-            Country
+          <td style="width: 1%;">
+            :
           </td>
-          <td
-            valign="top"
-            width="150px"
-          >
-            Custom permit
-          </td>
-          <td
-            valign="top"
-            width="150px"
-          >
-            Cargo Insurance
+          <td>
+            {{ incoming.from }}
           </td>
         </tr>
         <tr>
-          <td>{{ incoming.from }}</td>
-          <td>{{ incoming.from_country_name }}</td>
-          <td>{{ incoming.custom_permit }}</td>
-          <td>{{ incoming.cargo_insurance }}</td>
+          <td style="width: 10%">
+            Transport Type
+          </td>
+          <td style="width: 1%;">
+            :
+          </td>
+          <td>
+            {{ transportType }}
+          </td>
         </tr>
       </table>
-
-      <div style="margin-top: 20px;  margin-bottom: 20px; border: 0; border-top: 1px solid #eee;" />
+      <br>
       <div class="row">
         <div class="col-md-12">
           <table class="table table-bordered">
@@ -103,81 +108,150 @@
                 valign="top"
                 width="30px"
               >
-                #
+                No.
               </td>
               <td valign="top">
                 Product SKU
               </td>
               <td valign="top">
+                Product Name
+              </td>
+              <td valign="top">
                 Batch
               </td>
               <td valign="top">
-                Product Name
+                Product Location
+              </td>
+              <td valign="top">
+                Pallet ID
               </td>
               <td valign="top">
                 Location
               </td>
               <td valign="top">
-                Packing
-              </td>
-              <td valign="top">
                 Exp Date
               </td>
               <td valign="top">
+                UOM
+              </td>
+              <td valign="top">
                 Qty
+              </td>
+              <td valign="top">
+                WUn  ({{ nettWeightType }})
+              </td>
+              <td valign="top">
+                Nett Weight ({{ nettWeightType }})
+              </td>
+              <td valign="top">
+                Gross Weight ({{ grossWeightType }})
+              </td>
+              <td valign="top">
+                Volume ({{ volumeType }}) <sup>3</sup>
               </td>
             </tr>
             <tr
               v-for="(data, idx) in incoming.products"
               :key="data.id"
             >
-              <td valign="top">
+              <td
+                valign="top"
+              >
+                <input
+                  :v-model="nettWeightType = data.packing_type_weight_type"
+                  type="hidden"
+                >
+                <input
+                  :v-model="grossWeightType = data.packing_type_weight_type"
+                  type="hidden"
+                >
+                <input
+                  :v-model="volumeType = data.packing_type_dimension_type"
+                  type="hidden"
+                >
                 {{ idx + 1 }}
               </td>
               <td valign="top">
                 {{ data.product_sku }}
               </td>
               <td valign="top">
-                {{ data.batch }}
-              </td>
-              <td valign="top">
                 {{ data.product_name }}
               </td>
               <td valign="top">
-                {{ data.to_warehouse_location_name }} - Level : {{ data.to_warehouse_location_level }}
+                {{ data.batch }}
               </td>
               <td valign="top">
-                {{ data.product_packing_name }}
+                {{ data.unique_code }}
+              </td>
+              <td valign="top">
+                {{ data.id }}
+              </td>
+              <td valign="top">
+                {{ data.to_warehouse_location_name }}
               </td>
               <td valign="top">
                 {{ date(data.expired_date) }}
               </td>
               <td valign="top">
+                {{ data.product_packing_uom }}
+              </td>
+              <td valign="top">
                 {{ data.qty }}
+              </td>
+              <td valign="top">
+                {{ data.product_weight }}
+              </td>
+              <td valign="top">
+                {{ data.product_weight * data.qty }}
+              </td>
+              <td valign="top">
+                {{ (data.product_weight * data.qty) + data.packing_type_weight }}
+              </td>
+              <td valign="top">
+                {{ data.packing_type_length * data.packing_type_width * data.packing_type_height }}
               </td>
             </tr>
             <tr>
               <td
                 valign="top"
-                colspan="6"
-              /><td
-                valign="top"
                 align="right"
+                colspan="9"
               >
                 Total:
               </td>
               <td valign="top">
                 {{ totalQty }}
               </td>
+              <td valign="top">
+                {{ totalWUn }}
+              </td>
+              <td valign="top">
+                {{ totalNettWeight }}
+              </td>
+              <td valign="top">
+                {{ totalGrossWeight }}
+              </td>
+              <td valign="top">
+                {{ totalVolume }}
+              </td>
             </tr>
           </table>
         </div>
       </div>
       <br>
-      <br>
-      <div id="printfooter">
-        __________________________<br>Date and Signature
-      </div>
+      <table width="60%">
+        <tr>
+          <td>
+            PIC Warehouse,
+            <br><br><br><br>
+            __________________________<br>Date and Signature
+          </td>
+          <td>
+            Operator,<br><br><br><br>
+            __________________________<br>Date and Signature
+          </td>
+        </tr>
+      </table>
     </div>
   </client-only>
 </template>
@@ -189,12 +263,14 @@ export default {
   layout: 'admin-print',
   data () {
     return {
-      incoming     : [],
-      etd          : '',
-      transportType: '',
+      incoming       : [],
+      etd            : '',
+      transportType  : '',
+      nettWeightType : '',
+      grossWeightType: '',
+      volumeType     : '',
     }
   },
-
   computed: {
     totalQty: function () {
       if (this.incoming.products !== undefined) {
@@ -207,18 +283,62 @@ export default {
 
       return 0
     },
+    totalWUn: function () {
+      if (this.incoming.products !== undefined) {
+        let total = 0
+        this.incoming.products.forEach((value) => {
+          total = total + value.product_weight
+        })
+        return total
+      }
+
+      return 0
+    },
+    totalNettWeight: function () {
+      if (this.incoming.products !== undefined) {
+        let total = 0
+        this.incoming.products.forEach((value) => {
+          total = total + (value.product_weight * value.qty)
+        })
+        return total
+      }
+
+      return 0
+    },
+    totalGrossWeight: function () {
+      if (this.incoming.products !== undefined) {
+        let total = 0
+        this.incoming.products.forEach((value) => {
+          total = total + ((value.product_weight * value.qty) + value.packing_type_weight)
+        })
+        return total
+      }
+
+      return 0
+    },
+    totalVolume: function () {
+      if (this.incoming.products !== undefined) {
+        let total = 0
+        this.incoming.products.forEach((value) => {
+          total = total + (value.packing_type_length * value.packing_type_width * value.packing_type_height)
+        })
+        return total
+      }
+
+      return 0
+    },
   },
   async mounted () {
-    try {
+    await this.getData()
+    // window.print()
+  },
+  methods: {
+    async getData () {
       await this.$store.dispatch('incoming/getIncomingDetail', { idIncoming: atob(this.$route.params.id) })
       this.incoming      = this.$store.getters['incoming/getIncomingDetail'].result
       this.etd           = moment(this.incoming.etd).format('DD/MM/Y HH:mm')
       this.transportType = `${this.incoming.transport_type.charAt(0).toUpperCase()}${this.incoming.transport_type.slice(1)}`
-    } catch (error) {
-
-    }
-  },
-  methods: {
+    },
     date: function (date) {
       return moment(date).format('DD/MM/Y')
     },
